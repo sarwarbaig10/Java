20029981115ba209b2954a2dd8f41147b5e3c9b8
sarwarbaig10 <47021895+sarwarbaig10@users.noreply.github.com>
2019-02-18 14:19:58 +0530

    Add files via upload

---
+++ JVMConcepts1.java
@@ -0,0 +1,16 @@
+public class JVMConcepts1 
+
+{
+
+     
+public static void main(String []args)
+   
+  {
+       
+ 	System.out.println("Welcome to Java Programming");
+    
+    System.out.println("My name is: Sarwar Baig");
+   
+  }
+
+}
