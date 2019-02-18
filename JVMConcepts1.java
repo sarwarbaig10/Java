5f477530b4ae0cc859aa1ab74779aa44dbce40b8
sarwarbaig10 <47021895+sarwarbaig10@users.noreply.github.com>
2019-02-18 14:24:48 +0530

    Update JVMConcepts1.java

--- JVMConcepts1.java
+++ JVMConcepts1.java
@@ -1,16 +1,8 @@
 public class JVMConcepts1 
-
-{
-
-     
-public static void main(String []args)
-   
-  {
-       
- 	System.out.println("Welcome to Java Programming");
-    
-    System.out.println("My name is: Sarwar Baig");
-   
+{     
+public static void main(String []args) 
+  {  
+ 	System.out.println("Welcome to Java Programming"); 
+     System.out.println("My name is: Sarwar Baig");
   }
-
 }
