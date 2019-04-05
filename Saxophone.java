a1fc84f9e11c9cef82afcdb8989500c605d948d3
sarwarbaig10 <47021895+sarwarbaig10@users.noreply.github.com>
2019-04-05 10:10:42 +0530

    Create Saxophone.java

---
+++ Saxophone.java
@@ -0,0 +1,17 @@
+package music.wind;
+
+import music.Playable;
+
+public class Saxophone implements Playable {
+
+	public Saxophone() {
+		// TODO Auto-generated constructor stub
+	}
+
+	
+
+	public void play()
+	{
+		System.out.println("Saxophone is played");
+	}
+}
