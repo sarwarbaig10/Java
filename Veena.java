788bc9754dce109b5c3b70459f1cfb6029872b8e
sarwarbaig10 <47021895+sarwarbaig10@users.noreply.github.com>
2019-04-05 10:10:19 +0530

    Create Veena.java

---
+++ Veena.java
@@ -0,0 +1,17 @@
+package music.string;
+
+import music.Playable;
+public class Veena implements Playable
+{
+
+	public Veena() {
+		// TODO Auto-generated constructor stub
+	}
+
+	
+	public void play()
+	{
+		System.out.println("Veena is played");
+	}
+
+}
