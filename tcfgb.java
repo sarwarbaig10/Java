d62e00252e688c07b485c85a65f2e2d88682a298
sarwarbaig10 <47021895+sarwarbaig10@users.noreply.github.com>
2019-04-10 17:47:00 +0530

    Create tcfgb.java

---
+++ tcfgb.java
@@ -0,0 +1,26 @@
+
+import java.util.*; 
+
+public class tcfgb 
+{ 
+    public static void main(String[]args) 
+    { 
+        HashSet<String> h = new HashSet<String>(); 
+  
+        h.add("India"); 
+        h.add("Australia"); 
+        h.add("South Africa"); 
+        h.add("India"); 
+        System.out.println(h); 
+       
+        h.remove("Australia"); 
+        
+        System.out.println("List after removing Australia:"+h); 
+  
+         
+        System.out.println("Iterating over list:"); 
+        Iterator<String> i = h.iterator(); 
+        while (i.hasNext()) 
+            System.out.println(i.next()); 
+    } 
+} 
