abc7024ef5901ed3c705847fc70eb5cd9dfc5af6
sarwarbaig10 <47021895+sarwarbaig10@users.noreply.github.com>
2019-04-10 17:35:10 +0530

    Create claspp2.java

---
+++ claspp2.java
@@ -0,0 +1,15 @@
+import java.io.*;
+public class claspp2 
+{
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		String a=args[0];
+		String b=args[0];
+		if(args.length==2)
+		System.out.println(a+ "  Technolgies "+ b);
+		else
+			System.out.println("please enetr only 2 words");
+	}
+}
